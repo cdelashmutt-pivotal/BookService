dd8870b77eb6fd7b5b257bec3c6addff1a81a349
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-03-15 13:44:35 -0400

    Fixing service creation in setup script.

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
 Param (
+	[Parameter(Mandatory=$true, HelpMessage="The comma separated list of spaces to setup")]
 	[string[]]$spaces
 )
 
@@ -6,7 +7,7 @@ $currentSpace = (& cf target)[4] | foreach {($_ -split "\s+", 2)[1]}
 
 foreach($space in $spaces) {
 	& cf target -s $space
-	& cf create-service mssql-dev devBookServiceContext 
+	& cf create-service mssql-dev dev BookServiceContext 
 }
 
 & cf target -s $currentSpace
